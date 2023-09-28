05c87defbe2f8cf609c5322473d6bc921b85b0fd
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-28 19:11:23 +0200

    rješenje osnovno

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import CompA from "./components/CompA";
+import CompB from "./components/CompB";
+import CompC from "./components/CompC";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/compa">CompA</a>
+                    </li>
+                    <li>
+                        <a href="/compb">CompB</a>
+                    </li>
+                    <li>
+                        <a href="/compc">CompC</a>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="compa" element={<CompA />} />
+                <Route path="compb" element={<CompB />} />
+                <Route path="compc" element={<CompC />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
